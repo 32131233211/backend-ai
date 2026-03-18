7cb0a745f2107220d141d7dbb011ad16f38e7857
32131233211 <brangariza@gmail.com>
2026-03-18 16:51:00 -0500

    test key hardcoded

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post("/chat", async (req, res) => {
   },
   {
     headers: {
-      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+      "Authorization": "Bearer sk-xxxxxxxxxxxx"
     }
   }
 );
